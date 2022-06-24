57482dc8c5a20227fc529dfdc1c3922d55ce75ab
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-24 15:52:43 -0500

    adds player schema

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const axios = require('axios');
 
 app = express();
 
@@ -9,6 +10,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017", {useNewUrlParser: true})
+
+const playerSchema = {
+    account_id: Number,
+    name: String,
+    person_name: String,
+    mmr: Number,
+    avatar_full: String,
+    team: String,
+    rank: Number
+}
+
 
 foundData = [{name: "A", description: "B"}, {name:"C", description: "D"}]
 
